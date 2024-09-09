f6163fe9ce80c86e54102ba5c8032790f1227f07
Vedant Lahane <vedantanillahane@gmail.com>
2024-09-09 14:50:29 +0530

    Refactor TwoStackInOneArray class to improve stack operations

--- TwoStackOneArrayII.java
+++ TwoStackOneArrayII.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class TwoStackInOneArray {
+public class TwoStackOneArrayII {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
-        int top1 = 0;
-        int top2 = n/2;
+        int top1 = -1;
+        int top2 = n;
         while(true){
             System.out.println("1. Push in stack 1");
             System.out.println("2. Push in stack 2");
@@ -19,67 +19,67 @@ public class TwoStackInOneArray {
             int choice = sc.nextInt();
             switch(choice){
                 case 1:
-                    if(top1 == n/2){
+                    if(top1 == n/2 - 1){
                         System.err.println("Stack 1 is full");
                     }
                     else{
                         System.out.println("Enter the element to push in stack 1: ");
                         int ele = sc.nextInt();
-                        arr[top1] = ele;
                         top1++;
+                        arr[top1] = ele;
                     }
                     break;
                 case 2:
-                    if(top2 == n){
+                    if(top2 == n/2){
                         System.err.println("Stack 2 is full");
                     }
                     else{
                         System.out.println("Enter the element to push in stack 2: ");
                         int ele = sc.nextInt();
+                        top2--;
                         arr[top2] = ele;
-                        top2++;
                     }
                     break;
                 case 3:
-                    if(top1 == 0){
+                    if(top1 == -1){
                         System.err.println("Stack 1 is empty");
                     }
                     else{
-                        top1--;
                         System.out.println("Popped element from stack 1: " + arr[top1]);
+                        top1--;
                     }
                     break;
                 case 4:
-                    if(top2 == n/2){
+                    if(top2 == n){
                         System.err.println("Stack 2 is empty");
                     }
                     else{
-                        top2--;
                         System.out.println("Popped element from stack 2: " + arr[top2]);
+                        top2++;
                     }
                     break;
                 case 5:
-                    if(top1 == 0){
+                    if(top1 == -1){
                         System.err.println("Stack 1 is empty");
                     }
                     else{
                         System.out.println("Elements of stack 1 are: ");
-                        for(int i = 0; i < top1; i++){
+                        for(int i = 0; i <= top1; i++){
                             System.out.print(arr[i] + " ");
                         }
-                        System.out.println("\n");
+                        System.out.println();
                     }
                     break;
-                case 6: 
-                    if(top2 == n/2){
+                case 6:
+                    if(top2 == n){
                         System.err.println("Stack 2 is empty");
                     }
                     else{
                         System.out.println("Elements of stack 2 are: ");
-                        for(int i = n/2; i < top2; i++){
+                        for(int i = n - 1; i >= top2; i--){
                             System.out.print(arr[i] + " ");
                         }
-                        System.out.println("\n");
+                        System.out.println();
                     }
                     break;
                 case 7:
@@ -87,13 +87,9 @@ public class TwoStackInOneArray {
                     break;
                 default:
                     System.err.println("Invalid choice");
-            
             }
             //sc.close();
         }
-        
 
-
-        
     }
 }
