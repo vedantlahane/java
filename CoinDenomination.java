159a717417d10473b320f3c1cf27db9f4082a4da
Vedant Lahane <vedantanillahane@gmail.com>
2024-10-24 22:34:17 +0530

    Add CoinDenomination class for calculating the minimum number of coins required to make up a given amount

---
+++ CoinDenomination.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class CoinDenomination {
+    public static int coinChange( int coins[], int amount ){
+        int dp[][] = new int[coins.length+1][amount+1];
+        for( int i=0; i<=coins.length; i++ ){
+            dp[i][0] = 0;
+        }
+        int max = amount+1;
+        for( int i=1; i<=amount; i++ ){
+            dp[0][i] = max;
+        }
+        for( int i=1; i<=coins.length; i++ ){
+            for( int j=1; j<=amount; j++ ){
+                if( j<coins[i-1] ){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = Math.min( dp[i-1][j], dp[i][j-coins[i-1]]+1 );
+                }
+            }
+        }
+        return dp[coins.length][amount]>amount?-1:dp[coins.length][amount];
+
+    }
+    public static void main( String args[] ){
+        Scanner sc= new Scanner( System.in );
+        System.out.println("Enter the number of coins: ");
+        int n= sc.nextInt();
+        System.out.println("Enter the value of the coins: ");
+        int coins[] = new int[n];
+        for( int i=0; i<n; i++ ){
+            coins[i] = sc.nextInt();
+        }
+        System.out.println("Enter the amount: ");
+        int amount = sc.nextInt();
+        int result = coinChange( coins, amount );
+        System.out.println("The minimum number of coins required to make up the amount is: "+result);
+        sc.close();
+
+
+    }
+}
---
+++ MaxProfitJobScheduling.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.TreeMap;
+import java.util.Scanner;
+
+public class MaxProfitJobScheduling {
+    public static int jobScheduling( int startTime[], int endTime[], int profit[] ){
+        int n = startTime.length;
+        int jobs[][] = new int[n][3];
+        for( int i=0; i<n; i++ ){
+            jobs[i] = new int[]{ startTime[i], endTime[i], profit[i] };
+        }
+        Arrays.sort( jobs, (a,b)->a[1]-b[1] );
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        dp.put(0,0);
+        for( int job[]: jobs ){
+            int cur = dp.floorEntry( job[0] ).getValue() + job[2];
+            if( cur > dp.lastEntry().getValue() ){
+                dp.put( job[1], cur );
+            }
+        }
+        return dp.lastEntry().getValue();
+    }
+    public static void main( String args[] ){
+        Scanner sc= new Scanner( System.in );
+        System.out.println("Enter the number of jobs: ");
+        int n= sc.nextInt();
+        int startTime[] = new int[n];
+        int endTime[] = new int[n];
+        int profit[] = new int[n];
+        System.out.println("Enter the start time of the jobs: ");
+        for( int i=0; i<n; i++ ){
+            startTime[i] = sc.nextInt();
+        }
+        System.out.println("Enter the end time of the jobs: ");
+        for( int i=0; i<n; i++ ){
+            endTime[i] = sc.nextInt();
+        }
+        System.out.println("Enter the profit of the jobs: ");
+        for( int i=0; i<n; i++ ){
+            profit[i] = sc.nextInt();
+        }
+        int result = jobScheduling( startTime, endTime, profit );
+        System.out.println("The maximum profit that can be obtained is: "+result);
+        sc.close();
+    }
+}
