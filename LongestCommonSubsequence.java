adad21af62dca630d699fb066cecf0e3a73c54a0
Vedant Lahane <vedantanillahane@gmail.com>
2024-11-06 21:35:58 +0530

    Refactor LongestCommonSubsequence class to implement tabulation method for LCS calculation

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -30,20 +30,39 @@ public class LongestCommonSubsequence {
             return dp[ m ][ n ] = Math.max( lcs( s1.substring( 0, m - 1 ), s2, dp ), lcs( s1, s2.substring( 0, n - 1 ), dp ) );
         }
     }
-    public static void main( String args[] ){
-        Scanner sc = new Scanner( System.in );
-        System.out.println("Enter the first string: ");
-        String s1 = sc.next();
-        System.out.println("Enter the second string: ");
-        String s2 = sc.next();
-        System.out.println("The length of the longest common subsequence is: " + lcs( s1, s2 ) );
-        int dp[][] = new int[ s1.length() + 1 ][ s2.length() + 1 ];
-        for( int i = 0 ; i < s1.length() ; i++ ){
-            for( int j = 0 ; j < s2.length() ; j++){
-                dp[ i ][ j ] = -1;
+    public int lcs ( String s1, String s2, int m , int n){
+            int l[][] = new int[ m+1 ][ n+1 ];
+            for( int i = 0 ; i <= m ; i++ ){
+                for( int j = 0 ; j <= n ; j++ ){
+                    if ( i == 0 || j == 0 ){
+                        l[ i ][ j ] = 0;
+                    }
+                    else if ( s1.charAt( i - 1 ) == s2.charAt( j - 1 ) ){
+                        l[ i ][ j ] = 1 + l[ i - 1 ][ j - 1 ];
+                    }
+                    else{
+                        l[ i ][ j ] = Math.max( l[ i - 1 ][ j ], l[ i ][ j - 1 ] );
+                    }
+                }
             }
+            return l[ m ][ n ];
         }
-        System.out.println("The length of the longest common subsequence by memoization is: " + lcs( s1, s2, dp ) );
+        public static void main( String args[] ){
+            Scanner sc = new Scanner( System.in );
+            System.out.println("Enter the first string: ");
+            String s1 = sc.next();
+            System.out.println("Enter the second string: ");
+            String s2 = sc.next();
+            System.out.println("The length of the longest common subsequence is: " + lcs( s1, s2 ) );
+            int dp[][] = new int[ s1.length() + 1 ][ s2.length() + 1 ];
+            for( int i = 0 ; i < s1.length() ; i++ ){
+                for( int j = 0 ; j < s2.length() ; j++){
+                    dp[ i ][ j ] = -1;
+                }
+            }
+            System.out.println("The length of the longest common subsequence by memoization is: " + lcs( s1, s2, dp ) );
+            LongestCommonSubsequence l= new LongestCommonSubsequence();
+            System.out.println("The length of the longest common subsequence by tabulation is: " + l.lcs( s1, s2, s1.length(), s2.length() ) );
         
         sc.close();
     }
