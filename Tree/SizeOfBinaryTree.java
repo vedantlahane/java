b907326c0ac88d79feb3d3a0468103e464667682
Vedant Lahane <vedantanillahane@gmail.com>
2025-02-03 23:33:57 +0530

    Add SizeOfBinaryTree class to implement functionality for calculating the size of a binary tree

---
+++ Tree/SizeOfBinaryTree.java
@@ -0,0 +1,48 @@
+package Tree;
+import java.util.Scanner;
+public class SizeOfBinaryTree {
+    
+    public static class Node{
+        Node left;
+        Node right;
+        int data;
+        Node(int data){
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static Node insert(Node root, int data){
+        if(root == null) return new Node(data);
+        else{
+            Node curr;
+            if( data <= root.data){
+                curr = insert(root.left, data);
+                root.left = curr;
+            }
+            else{
+                curr = insert(root.right, data);
+                root.right = curr;
+            }
+        }
+        return root;
+    }
+
+    public static int size(Node root){
+        if(root == null) return 0;
+        return 1 + size(root.left) + size(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node root = null;
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        System.out.println(size(root));
+        sc.close();
+    }
+    
+}
