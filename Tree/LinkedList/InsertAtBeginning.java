b6084738fcc72e7d724e08364a197722320fa413
Vedant Lahane <vedantanillahane@gmail.com>
2025-02-04 23:11:15 +0530

    Add InsertAtBeginning class to implement insertion at the beginning of a linked list

---
+++ Tree/LinkedList/InsertAtBeginning.java
@@ -0,0 +1,39 @@
+package Tree.LinkedList;
+import java.util.Scanner;
+public class InsertAtBeginning {
+    public static class Node{
+        Node next;
+        int data;
+        Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+    public static Node insert(Node head, int data){
+        Node temp = new Node(data);
+        temp.next = head;
+        return temp;
+    }
+
+    public static void print(Node head){
+        Node curr = head;
+        while( curr != null ){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+
+    public static void main( String args[]){
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int t = sc.nextInt();
+        while (t-- > 0){
+            int data = sc.nextInt();
+            head = insert(head, data);
+            
+        }
+        print(head);
+        sc.close();
+        
+    }
+}
