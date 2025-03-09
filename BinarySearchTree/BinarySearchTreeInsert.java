93b4c2ec1041c39cad069190d3e58decb491720a
Vedant Lahane <vedantanillahane@gmail.com>
2025-03-09 23:07:49 +0530

    Add insert and inorder methods to BinarySearchTreeInsert class for BST functionality

--- BinarySearchTree/BinarySearchTreeInsert.java
+++ BinarySearchTree/BinarySearchTreeInsert.java
@@ -13,7 +13,39 @@ public class BinarySearchTreeInsert{
         
         public Node insert(Node root, int data){
             if(root == null || root.data == data) return root;
+            if(data < root.data){
+                root.left = insert(root.left, data);
+            }else{
+                root.right = insert(root.right, data);
+            }
+            return root;
+
         }
+
+        public void inorder(Node root){
+            if(root == null) return;
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+
     }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTreeInsert bst = new BinarySearchTreeInsert();
+        Node root = bst.new Node(10);
+        root.insert(root, 5);
+        root.insert(root, 15);
+        root.insert(root, 2);
+        root.insert(root, 7);
+        root.insert(root, 12);
+        root.insert(root, 20);
+        root.inorder(root);
+        sc.close();
+    }
+
+    
 }
 
