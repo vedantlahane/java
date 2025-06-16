b664e7fb1c6ee822c595d2a9be81d5e47d9e0667
Vedant Lahane <vedantanillahane@gmail.com>
2025-06-16 11:49:21 +0530

    Refactor student management operations for improved clarity and validation

--- StudentMarksManagementSystemUsingArrays.java
+++ StudentMarksManagementSystemUsingArrays.java
@@ -7,43 +7,42 @@ public class StudentMarksManagementSystemUsingArrays {
         int n = sc.nextInt();
         String[] names = new String[n];
         int[] marks = new int[n];
-
-        System.out.println("Enter the operation you have to do:");
-        System.out.println("1. Add Student");
-        System.out.println("2. Remove Student");
-        System.out.println("3. Update Marks");
-        System.out.println("4. Display Students");
-        System.out.println("5. Exit");
-
         int ind = 0;
         boolean flag = true;
+
         while (flag) {
-            switch (sc.nextInt()) {
+            System.out.println("\nChoose an operation:");
+            System.out.println("1. Add Student");
+            System.out.println("2. Remove Student");
+            System.out.println("3. Update Marks");
+            System.out.println("4. Display Students");
+            System.out.println("5. Exit");
+
+            int choice = sc.nextInt();
+            sc.nextLine(); // Consume newline
+
+            switch (choice) {
                 case 1:
+                    if (ind >= n) {
+                        System.out.println("Cannot add more students, array is full.");
+                        break;
+                    }
                     System.out.println("Enter the name of the student:");
-                    sc.nextLine(); // Consume the newline character
                     String name = sc.nextLine();
-                    System.out.println("Enter the marks of the student:");
+                    System.out.println("Enter the marks of the student (1-100):");
                     int mark = sc.nextInt();
-                    if (mark > 0 && mark <= 100) {
-                        if (ind < n) {
-                            names[ind] = name;
-                            marks[ind] = mark;
-                            ind++;
-                            System.out.println("Student added successfully.");
-                        } else {
-                            System.out.println("Cannot add more students, array is full.");
-                        }
+                    if (mark >= 1 && mark <= 100) {
+                        names[ind] = name;
+                        marks[ind] = mark;
+                        ind++;
+                        System.out.println("Student added successfully.");
                     } else {
-                        System.out.println("Marks should be between 1 and 100.");
-                        mark = sc.nextInt();
-                        marks[ind--] = mark;
-                        System.out.println("Student added successfully");
+                        System.out.println("Invalid marks. Student not added.");
                     }
                     break;
+
                 case 2:
                     System.out.println("Enter the name of student to remove:");
-                    sc.nextLine(); // Consume the newline character
                     String removeName = sc.nextLine();
                     boolean found = false;
                     for (int i = 0; i < ind; i++) {
@@ -53,58 +52,62 @@ public class StudentMarksManagementSystemUsingArrays {
                                 names[j] = names[j + 1];
                                 marks[j] = marks[j + 1];
                             }
-                            names[ind - 1] = null; // Clear the last element
-                            marks[ind - 1] = 0; // Reset the marks
+                            names[ind - 1] = null;
+                            marks[ind - 1] = 0;
                             ind--;
                             System.out.println("Student removed successfully.");
-
+                            break;
                         }
                     }
-
                     if (!found) {
                         System.out.println("Student not found.");
                     }
-
                     break;
 
                 case 3:
                     System.out.println("Enter the name of student to update marks:");
-                    sc.nextLine(); // Consume the newline character
                     String updateName = sc.nextLine();
                     boolean updated = false;
                     for (int i = 0; i < ind; i++) {
                         if (names[i].equals(updateName)) {
-                            System.out.println("Enter the new marks:");
-                            marks[i] = sc.nextInt();
+                            System.out.println("Enter the new marks (1-100):");
+                            int newMarks = sc.nextInt();
+                            if (newMarks >= 1 && newMarks <= 100) {
+                                marks[i] = newMarks;
+                                System.out.println("Marks updated successfully.");
+                            } else {
+                                System.out.println("Invalid marks. Update cancelled.");
+                            }
                             updated = true;
-                            System.out.println("Marks updated successfully.");
-
+                            break;
                         }
                     }
-
                     if (!updated) {
                         System.out.println("Student not found.");
                     }
-
                     break;
 
                 case 4:
                     System.out.println("Displaying all students:");
-                    for (int i = 0; i < ind; i++) {
-                        if (names[i] != null) {
+                    if (ind == 0) {
+                        System.out.println("No students to display.");
+                    } else {
+                        for (int i = 0; i < ind; i++) {
                             System.out.println("Name: " + names[i] + ", Marks: " + marks[i]);
                         }
                     }
                     break;
+
                 case 5:
                     System.out.println("Exiting the program.");
-                    sc.close();
                     flag = false;
+                    break;
 
                 default:
                     System.out.println("Invalid operation. Please try again.");
-
             }
         }
+
+        sc.close();
     }
 }
