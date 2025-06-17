4019d28c6840b6d8cabf4608d929c4465f8bb2eb
Vedant Lahane <vedantanillahane@gmail.com>
2025-06-17 10:42:20 +0530

    Reintroduce IPAddressValidOrNot class with IP address validation logic

--- IPAddressValidOrNot.java
+++ IPAddressValidOrNot.java
@@ -1,5 +1,5 @@
 import  java.util.*;
-public class IPAdressValidOrNot {
+public class IPAddressValidOrNot {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the IP address:");
