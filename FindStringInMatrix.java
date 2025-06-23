6cd6fa66f84cb8b320c028b57da5921a3426622d
Vedant Lahane <vedantanillahane@gmail.com>
2025-06-23 13:44:38 +0530

    Refactor FindStringInMatrix for improved readability and formatting

--- FindStringInMatrix.java
+++ FindStringInMatrix.java
@@ -6,22 +6,23 @@
 // words to find "air" and "pea"
 
 import java.util.*;
+
 public class FindStringInMatrix {
 
-    public static boolean findWordInMatrix(char[][] matrix, String word){
+    public static boolean findWordInMatrix(char[][] matrix, String word) {
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(checkDirection(matrix, word, i, j, 0, 1) || // Right
-                   checkDirection(matrix, word, i, j, 1, 0) || // Down
-                   checkDirection(matrix, word, i, j, 0, -1) || // Left
-                   checkDirection(matrix, word, i, j, -1, 0) || // Up
-                   checkDirection(matrix, word, i, j, 1, 1) || // Down-Right
-                   checkDirection(matrix, word, i, j, -1, -1) || // Up-Left
-                   checkDirection(matrix, word, i, j, -1, 1) || // Up-Right
-                   checkDirection(matrix, word, i, j, 1, -1)) { // Down-Left
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (checkDirection(matrix, word, i, j, 0, 1) || // Right
+                        checkDirection(matrix, word, i, j, 1, 0) || // Down
+                        checkDirection(matrix, word, i, j, 0, -1) || // Left
+                        checkDirection(matrix, word, i, j, -1, 0) || // Up
+                        checkDirection(matrix, word, i, j, 1, 1) || // Down-Right
+                        checkDirection(matrix, word, i, j, -1, -1) || // Up-Left
+                        checkDirection(matrix, word, i, j, -1, 1) || // Up-Right
+                        checkDirection(matrix, word, i, j, 1, -1)) { // Down-Left
                     return true;
                 }
             }
@@ -29,25 +30,29 @@ public class FindStringInMatrix {
         return false;
     }
 
-    public static boolean checkDirection(char[][] matrix, String word, int startRow, int startCol, int rowDir, int colDir) {
+    public static boolean checkDirection(char[][] matrix, String word, int startRow, int startCol, int rowDir,
+            int colDir) {
         int rows = matrix.length;
         int cols = matrix[0].length;
         int wordLength = word.length();
 
-        for(int k = 0; k < wordLength; k++) {
+        for (int k = 0; k < wordLength; k++) {
             int newRow = startRow + k * rowDir;
             int newCol = startCol + k * colDir;
 
             // Check if the new position is out of bounds
-            if(newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) return false;
+            if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols)
+                return false;
 
             // Check if the character matches
-            if(matrix[newRow][newCol] != word.charAt(k)) return false;
-            
+            if (matrix[newRow][newCol] != word.charAt(k))
+                return false;
+
         }
         return true;
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows in the matrix: ");
         int rows = sc.nextInt();
@@ -55,16 +60,16 @@ public class FindStringInMatrix {
         int cols = sc.nextInt();
         char[][] matrix = new char[rows][cols];
         System.out.println("Enter the matrix elements row by row:");
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 matrix[i][j] = sc.next().charAt(0);
             }
         }
         System.out.println("Enter the words to find (space-separated): ");
         sc.nextLine();
         String[] words = sc.nextLine().split(" ");
-        for(String word : words) {
-            if(findWordInMatrix(matrix, word)) {
+        for (String word : words) {
+            if (findWordInMatrix(matrix, word)) {
                 System.out.println("Word '" + word + "' found in the matrix.");
             } else {
                 System.out.println("Word '" + word + "' not found in the matrix.");
